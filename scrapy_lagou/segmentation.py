f416ff33f64b60a1eb65c3df00bb95aa4a62337a
lionrock <xunli.xyz@gmail.com>
2016-05-11 17:53:11 +0800

    add scrapy

---
+++ scrapy_lagou/segmentation.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import sys
+import jieba
+import pymysql.cursors
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class WordSeg(object):
+
+    db = 'lagou'
+    user = 'root'
+    passwd = ''
+
+    def __init__(self):
+        self.dbc = pymysql.connect(
+                user = self.user,
+                passwd = self.passwd,
+                host = 'localhost',
+                database = self.db
+                )
+        self.cursor = self.dbc.cursor()
+        self.cursor.execute('set names utf8')
+        self.load_ignoring()
+
+    # XXX this class should not hold other objects,
+    # or else it will not be called.
+    # see this link for details:
+    # http://eli.thegreenplace.net/2009/06/12/safely-using-destructors-in-python
+    def __del__(self):
+        self.cursor.close()
+        self.dbc.close()
+
+    def load_ignoring(self):
+        query = "select word from ignored_word"
+        self.cursor.execute(query)
+        ignored = {}
+        for word in self.cursor:
+            ignored[word[0]] = 1
+        self.ignored_words = ignored
+        # print self.ignored_words
+
+    def segment(self):
+        query = "select distinct search_keyword from position"
+        self.cursor.execute(query)
+        keywords = [kw[0] for kw in self.cursor]
+        for kw in keywords:
+            self.segment_one(kw)
+
+    def segment_one(self, keyword):
+        print 'process keyword "%s"' % keyword
+
+        # XXX
+        # `execute' has a nasty bug of string formatting of mysql connector,
+        # when using `%s' and the keyword is 'C++'
+        query = '''
+        select job_desc from job_desc jd
+        inner join position p on jd.position_id = p.position_id
+        where p.search_keyword = %s
+        '''
+
+        self.cursor.execute(query, (keyword))
+
+        all_jd = ''
+        for jd in self.cursor:
+            all_jd = all_jd + jd[0]
+
+        counter = {}
+        for seg in jieba.cut(all_jd):
+            seg = seg.lower()
+            if seg in self.ignored_words:
+                continue
+            if seg not in counter:
+                counter[seg] = 0
+            else:
+                counter[seg] = counter[seg] + 1
+
+        for (word, cnt) in counter.iteritems():
+            self.cursor.execute(
+                    "insert into word_frequency (search_keyword, word, cnt) values (%s, %s, %s)",
+                    (keyword, word.encode('utf8', 'ignore'), cnt)
+                )
+
+if '__main__' == __name__:
+    ws = WordSeg()
+    ws.segment()
---
+++ scrapy_lagou/spiders/lagou_spider.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+import sys
+import scrapy
+import json
+import re
+import urllib
+from scrapy_lagou.items import LagouPositionItem, LagouJobDescItem
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class LagouSpider(scrapy.Spider):
+    name = "lagou"
+    allowed_domains = ["lagou.com"]
+    # FIXME remove hard-coding
+    keyword = 'C'  # candicates: C, C++, Python, PHP
+    city = u'深圳'
+    pn = 1  # page no.
+
+    def start_requests(self):
+        return [
+            scrapy.FormRequest(
+                url="http://www.lagou.com/jobs/positionAjax.json?city=北京",
+                headers={'Referer': 'http://www.lagou.com/jobs/list_Java?px=default&city=北京'},
+                formdata={
+                    'kd': self.keyword,
+                    'first': 'false',
+                    'pn': str(self.pn)
+                },
+                callback=self.parse
+            ),
+            scrapy.FormRequest(
+                url="http://www.lagou.com/jobs/positionAjax.json?city=上海",
+                headers={'Referer': 'http://www.lagou.com/jobs/list_Java?px=default&city=上海'},
+                formdata={
+                    'kd': self.keyword,
+                    'first': 'false',
+                    'pn': str(self.pn)
+                },
+                callback=self.parse
+            ),
+            scrapy.FormRequest(
+                url="http://www.lagou.com/jobs/positionAjax.json?city=深圳",
+                headers={'Referer': 'http://www.lagou.com/jobs/list_Java?px=default&city=%E6%B7%B1%E5%9C%B3'},
+                formdata={
+                    'kd': self.keyword,
+                    'first': 'false',
+                    'pn': str(self.pn)
+                },
+                callback=self.parse
+            ),
+        ]
+
+    def parse(self, resp):
+        js = json.loads(resp.body_as_unicode())
+        if not js['success']:
+            self.logger.error('failed to get json')
+        else:
+            # print('#########################')
+            # print '一共有%d页数据' % js['content']['pageSize']   # 一共有多少页
+            # print('#########################')
+
+            # f = open('../log.json', 'w')
+            # f.write(str(js).encode('utf8', 'ignore'))
+            # f.close()
+
+            for i in range(js['content']['pageSize']):
+                json_item = js['content']['positionResult']['result'][i]
+                position = LagouPositionItem()
+                position['search_keyword'] = self.keyword
+                position['company_short'] = json_item['companyName']
+                position['company'] = json_item['companyShortName']
+                position['company_id'] = json_item['companyId']
+                position['company_size'] = json_item['companySize']
+                position['education'] = json_item['education']
+                position['finance_stage'] = json_item['financeStage']
+                position['industry'] = json_item['industryField']
+                position['city'] = self.city
+                position['position_type'] = json_item['positionType']
+                position['position_name'] = json_item['positionName']
+                position['position_id'] = json_item['positionId']
+                position['advantage'] = json_item['positionAdvantage']
+                position['salary'] = json_item['salary']
+                position['work_year'] = json_item['workYear']
+                yield position
+
+                yield scrapy.Request('http://www.lagou.com/jobs/' + str(json_item['positionId']) + '.html',
+                                     callback=self.parse_job_desc
+                                     )
+
+        self.pn = self.pn + 1
+        if self.pn > js['content']['totalPageCount']:
+            self.logger.info('Finished crawling %s pages of json feeds' %
+                             js['content']['totalPageCount'])
+            return
+        # FIXME avoid duplicate
+        yield scrapy.FormRequest(
+            "http://www.lagou.com/jobs/positionAjax.json?city=" + urllib.quote(self.city.encode('utf8')),
+            formdata={'kd': self.keyword, 'first': 'false', 'pn': str(self.pn)},
+            callback=self.parse
+        )
+
+    # 解析职位描述html页面
+    def parse_job_desc(self, resp):
+        jd = LagouJobDescItem()
+        p = re.compile('[^0-9]+([0-9]+)\.html')
+        jd['position_id'] = int(p.sub(r'\1', resp.url))
+
+        jd['dept'] = ''.join(
+            resp.xpath('//*[@id="container"]/div[1]/dl[1]/dt/h1/div/text()').extract()
+        ).strip().encode('utf8', 'ignore')
+
+        subtree_root = resp.xpath('//*[@id="container"]/div[1]/dl[1]/dd[2]')[0]
+
+        jd['job_desc'] = ''.join(
+            [text.extract().strip() for text in subtree_root.xpath('.//text()')]
+        ).strip().encode('utf8', 'ignore')
+        jd['job_responsibility'] = ''
+        jd['job_requirement'] = ''
+        yield jd
+
+    def closed(self, reason):
+        self.logger.info(("Please run script `./scrapy_lagou/segmentation.py' "
+                          "to do word segmentation and then calculate frequencies of every words"))
